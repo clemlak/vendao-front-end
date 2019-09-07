af24b36ca8da7aca612fbc19fa3a1f483d223597
Clement Lakhal <clementlakhal@gmail.com>
2019-09-07 02:10:56 -0400

    Update

--- src/components/product/index.stories.jsx
+++ src/components/product/index.stories.jsx
@@ -6,7 +6,6 @@ import Product from '.';
 storiesOf('Components/Product', module)
   .add('Standard', () => (
     <Product
-      label="Dr Pepper"
       url="https://cdn.shoplightspeed.com/shops/621581/files/14012574/800x1024x2/dr-pepper-dr-pepper-12-oz-4-6-pk.jpg"
       price="2"
       name="Dr Pepper"
--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -5,44 +5,64 @@ import {
 } from 'reflexbox';
 
 import Product from '../../components/product';
+import Products from '../../common/products';
+
+function displayProducts(products) {
+  console.log('Products length:', products.length);
+  console.log('Products length % 4:', products.length / 4);
+
+
+  const display = [];
+
+  for (let i = 0; i < products.length / 4; i += 1) {
+    const rows = [];
+
+    for (let j = 0; j < 4; j += 1) {
+      rows.push(
+        <Box width={[1, 1 / 2, 1 / 3, 1 / 4]} p={3} key={i + j}>
+          <Product
+            name={products[i + j].name}
+            url={products[i + j].url}
+            price={products[i + j].price}
+          />
+        </Box>,
+      );
+    }
+
+    display.push(
+      <Flex>
+        {rows}
+      </Flex>,
+    );
+  }
+
+  for (let i = 0; i < products.length % 4; i += 4) {
+    const rows = [];
+
+    for (let j = 0; j < products.length % 4; j += 1) {
+      rows.push(
+        <Box width={[1, 1 / 2, 1 / 3, 1 / 4]} p={3} key={i + j * 2}>
+          <Product
+            name={products[i + j].name}
+            url={products[i + j].url}
+            price={products[i + j].price}
+          />
+        </Box>,
+      );
+    }
+
+    display.push(
+      <Flex>
+        {rows}
+      </Flex>,
+    );
+  }
+
+  return display;
+}
 
 function Home() {
-  return (
-    <Flex>
-      <Box width={1 / 4} p={3}>
-        <Product
-          label="Dr Pepper"
-          url="https://cdn.shoplightspeed.com/shops/621581/files/14012574/800x1024x2/dr-pepper-dr-pepper-12-oz-4-6-pk.jpg"
-          price="2"
-          name="Dr Pepper"
-        />
-      </Box>
-      <Box width={1 / 4} p={3}>
-        <Product
-          label="Dr Pepper"
-          url="https://cdn.shoplightspeed.com/shops/621581/files/14012574/800x1024x2/dr-pepper-dr-pepper-12-oz-4-6-pk.jpg"
-          price="2"
-          name="Dr Pepper"
-        />
-      </Box>
-      <Box width={1 / 4} p={3}>
-        <Product
-          label="Dr Pepper"
-          url="https://cdn.shoplightspeed.com/shops/621581/files/14012574/800x1024x2/dr-pepper-dr-pepper-12-oz-4-6-pk.jpg"
-          price="2"
-          name="Dr Pepper"
-        />
-      </Box>
-      <Box width={1 / 4} p={3}>
-        <Product
-          label="Dr Pepper"
-          url="https://cdn.shoplightspeed.com/shops/621581/files/14012574/800x1024x2/dr-pepper-dr-pepper-12-oz-4-6-pk.jpg"
-          price="2"
-          name="Dr Pepper"
-        />
-      </Box>
-    </Flex>
-  );
+  return displayProducts(Products);
 }
 
 export default Home;
